4fd4640c21da6fde077a67beafe856ceecc18ed1
Chen Codes <chencodes@gmail.com>
2020-04-15 19:15:46 -0700

    Initial Commit

---
+++ ChenCodesIntroductionToSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChenCodesIntroductionToSwiftUI
+//
+//  Created by Chen Codes on 4/15/20.
+//  Copyright © 2020 Chen Codes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
